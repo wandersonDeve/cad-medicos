749ce2f7b64690ed85a209be87179f4cd7d6e807
Wanderson Santos <wanderlavo@gmail.com>
2022-02-25 14:29:03 -0300

    CRUD inicial de medicos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MedicoModule } from './medico/medico.module';
 
 @Module({
-  imports: [],
+  imports: [MedicoModule],
   controllers: [AppController],
   providers: [AppService],
 })
